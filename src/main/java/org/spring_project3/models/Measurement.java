d7e9b71962921e6c3fd486f2614846a2e555d586
MaximKiel <loup.du.nord@yandex.ru>
2023-10-11 20:54:47 +0300

    Implement model classes and initDB.sql

--- src/main/java/org/spring_project3/models/Measurement.java
+++ src/main/java/org/spring_project3/models/Measurement.java
@@ -1,4 +1,85 @@
 package org.spring_project3.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "measurement")
 public class Measurement {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "value")
+    @NotNull
+    @Min(value = -100, message = "Value should be more than -100")
+    @Max(value = 100, message = "Value should be less than 100")
+    private double value;
+
+    @Column(name = "raining")
+    @NotNull
+    private boolean raining;
+
+    @Column(name = "measure_at")
+    @NotNull
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private LocalDateTime measureAt;
+
+    @ManyToOne
+    @JoinColumn(name = "sensor_id", referencedColumnName = "id")
+    private Sensor sensor;
+
+    public Measurement() {
+    }
+
+    public Measurement(double value, boolean raining) {
+        this.value = value;
+        this.raining = raining;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(boolean raining) {
+        this.raining = raining;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public LocalDateTime getMeasureAt() {
+        return measureAt;
+    }
+
+    public void setMeasureAt(LocalDateTime measureAt) {
+        this.measureAt = measureAt;
+    }
 }
