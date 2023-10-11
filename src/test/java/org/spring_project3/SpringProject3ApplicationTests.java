8d27db32f9c7d68d5ee13dc125405c9e5aabd935
MaximKiel <loup.du.nord@yandex.ru>
2023-10-11 20:20:42 +0300

    Create empty project

---
+++ src/test/java/org/spring_project3/SpringProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring_project3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
